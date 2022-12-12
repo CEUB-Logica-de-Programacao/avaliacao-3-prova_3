07c69c2d71803cc984e1c0473d9b3e54a382c867
Henriqwee <111283290+Henriqwee@users.noreply.github.com>
2022-12-12 10:42:45 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -21,7 +21,7 @@
 #
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
-def q1(heights):
+def q1(altura):
     count = 0 
     maioraltura = 0 
     for alturas in altura: 
