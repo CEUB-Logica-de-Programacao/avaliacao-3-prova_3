c1b72d69f5c4123ba5e2e4eb29c69016bf689734
Henriqwee <111283290+Henriqwee@users.noreply.github.com>
2022-12-12 12:27:50 -0300

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -26,27 +26,28 @@
 
 def bonus(n):
     def printParenteses(str, n):
-    if(n > 0):
-        _printParenteses(str, 0,
-                          n, 0, 0)
-    return
-def _printParenteses(str, pos, n,
-                      open, close):
-    if(close == n):
-        for i in str:
-            print(i, end="")
-        print()
+        
+        if(n > 0):
+            _printParenteses(str, 0,
+                            n, 0, 0)
         return
-    else:
-        if(open > close):
-            str[pos] = ')'
-            _printParenteses(str, pos + 1, n,
-                              open, close + 1)
-        if(open < n):
-            str[pos] = '('
-            _printParenteses(str, pos + 1, n,
-                              open + 1, close)                            
-str = [""] * 2 * n
-printParenteses(str, n)
+    def _printParenteses(str, pos, n,
+                      open, close):
+        if(close == n):
+            for i in str:
+                print(i, end="")
+            print()
+            return
+        else:
+            if(open > close):
+                str[pos] = ')'
+                _printParenteses(str, pos + 1, n,
+                                open, close + 1)
+            if(open < n):
+                str[pos] = '('
+                _printParenteses(str, pos + 1, n,
+                                open + 1, close)                            
+    str = [""] * 2 * n
+    printParenteses(str, n)
 if __name__ == '__main__':
     print(bonus(10))
