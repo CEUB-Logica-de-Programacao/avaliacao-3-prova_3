44d602849a77eb25d5c0b2dcfb48f5f93769d8fd
Henriqwee <111283290+Henriqwee@users.noreply.github.com>
2022-12-12 12:17:44 -0300

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -25,7 +25,29 @@
 # Para obter a nota máxima dessa questão, não deve-se utilizar nenhuma função pronta do Python.
 
 def bonus(n):
-    # Escreva seu código aqui
+    def printParenteses(str, n):
+    if(n > 0):
+        _printParenteses(str, 0,
+                          n, 0, 0)
+    return
+def _printParenteses(str, pos, n,
+                      open, close):
+    if(close == n):
+        for i in str:
+            print(i, end="")
+        print()
+        return
+    else:
+        if(open > close):
+            str[pos] = ')'
+            _printParenteses(str, pos + 1, n,
+                              open, close + 1)
+        if(open < n):
+            str[pos] = '('
+            _printParenteses(str, pos + 1, n,
+                              open + 1, close)
+str = [""] * 2 * n
+printParenteses(str, n)
     return []
 
 
