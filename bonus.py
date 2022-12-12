b1d80ca1cc7e9a43277e8b9ab181c0c6425b6351
Henriqwee <111283290+Henriqwee@users.noreply.github.com>
2022-12-12 12:58:54 -0300

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -25,29 +25,7 @@
 # Para obter a nota máxima dessa questão, não deve-se utilizar nenhuma função pronta do Python.
 
 def bonus(n):
-    def gerarParenteses(n,  Open,  close,  s, ans):
- 
-    if(Open == n and close == n):
-        ans.append(s)
-        return
- 
-
-    if(Open < n):
-        gerarParenteses(n, Open+1, close, s+"(", ans)
- 
-  
-    if(close < Open):
-        gerarParenteses(n, Open, close + 1, s+")", ans)
- 
- 
-n = 3
- 
-
-ans = []
- 
-gerarParenteses(n, 0, 0, "", ans)
- 
-for s in ans:
-    print(s)
+    # Escreva seu código aqui
+    return []
 if __name__ == '__main__':
     print(bonus(10))
