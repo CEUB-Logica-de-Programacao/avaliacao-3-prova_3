fe1b39b129c01fe31d00acc33d62a90d879d3a07
Henriqwee <111283290+Henriqwee@users.noreply.github.com>
2022-12-12 11:10:54 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,26 +1,3 @@
-# ## Questão 1
-#
-# Você foi contratado pela seleção brasileira para fazer uma análise sobre as seleções adversárias. Sua primeira tarefa
-# consiste em realizar uma contagem de quantos jogadores possuem a altura máxima.
-#
-# ### Exemplo
-#
-# Para a entrada:
-#
-# ```
-# [180, 166, 170, 180]
-# ```
-#
-# A saída deve ser:
-#
-# ```
-# 2
-# ```
-#
-# Isso porque existem dois jogadores com altura máxima (180).
-#
-# Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
-
 def q1(heights):
  count = 0
  maior = 0 
