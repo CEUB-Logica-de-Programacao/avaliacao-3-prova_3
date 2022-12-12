bcefec233a24f835b2a244ffb1e237c6ec1233c9
Henriqwee <111283290+Henriqwee@users.noreply.github.com>
2022-12-12 10:54:53 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -21,16 +21,17 @@
 #
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
-def q1(altura):
-    count = 0 
-    maioraltura = 0 
-    for alturas in altura: 
-        if alturas > maioraltura: 
-            maioraltura = alturas 
-            count *= 0 
-            if maioraltura == alturas:
-                count +=1
-    return count
+def q1(heights): 
+     count = 0 
+     maior = 0 
+     for alturas in heights: 
+         if alturas > maior: 
+             maior = item 
+             count *= 0 
+         if maior == alturas: 
+             count +=1 
+          
+     return count
 
 
 if __name__ == '__main__':
